d2658d3dd16ca2e513be7163af9dc4af0dcb2263
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2025-01-05 12:39:25 -0800

    Add files via upload

---
+++ AXI-Stream Biquad/iir_DF1_Biquad_AXIS.v
@@ -0,0 +1,218 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Dominic Meads
+// 
+// Create Date: 11/09/2024 10:05:43 PM
+// Design Name: 
+// Module Name: iir_DF1_Biquad_AXIS
+// Project Name: 
+// Target Devices: 7 Series
+// Tool Versions: Vivado 2020.2
+// Description: 
+//      IIR Direct-form I structure BiQuad. Compatible with AXI4 streaming interface with parameterized 
+//      coefficients. Example is in Q1.14 format I think? (coefficients are multiplied by 2^14, see below) 
+//
+//      IMPORTANT: input/output data width and coefficient width are adjustable in the parameters. Xilinx 7 Series DSP48E1 tile has
+//                 a 25x18 bit multiplier, do not exceed this width in the parameters. E.g. max coeff_width = 25 and max inout_width = 18, 
+//                 or vice versa. 
+//
+//      Example: lowpass elliptical filter. Cuttoff frequency of 1.5 MHz (Fsample = 10 MHz), with 40 dB stopband attenuation and 0.5 dB ripple in the passband
+//        see MATLAB Script here: https://github.com/dominic-meads/Vivado-Projects/blob/main/IIR_Direct_form_1_Biquad/sample_gen.m
+//
+//        filter coefficients generated in MATLAB (multiplied floating point coefficients by 2^14)
+//  
+//        sos = {1.0000    1.8955    1.0000    1.0000   -0.5714    0.3176}
+//                 b0        b1        b2        a0        a1        a2
+//          g = 0.1808 
+//  
+//        parameter coeff_width  = 16,     // coefficient bit width
+//        parameter inout_width  = 16,     // input and output data wdth
+//        parameter scale_factor = 14,     // multiplying coefficients by 2^14
+//        parameter b0_int_coeff = 2962,   // g * b0 * 2^14  (multiply denom coeffs by gain for DF1 [source 1])
+//        parameter b1_int_coeff = 5615,   // g * b1 * 2^14 
+//        parameter b2_int_coeff = 2962    // g * b2 * 2^14
+//        parameter a1_int_coeff = -9362,  // a1 * 2^14
+//        parameter a2_int_coeff = 5203,   // a2 * 2^14
+//
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//      Source 1: https://ccrma.stanford.edu/~jos/filters/BiQuad_Section.html#:~:text=The%20term%20%60%60biquad%27%27%20is%20short%20for%20%60%60bi-quadratic%27%27%2C%20and,be%20called%20the%20overall%20gain%20of%20the%20biquad.
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module iir_DF1_Biquad_AXIS #(
+  parameter coeff_width  = 16,     // coefficient bit width
+  parameter inout_width  = 16,     // input and output data wdth
+  parameter scale_factor = 14,     // multiplying coefficients by 2^14
+  parameter b0_int_coeff = 2962,   // integer coefficients
+  parameter b1_int_coeff = 5615,
+  parameter b2_int_coeff = 2962,
+  parameter a1_int_coeff = -9362,  
+  parameter a2_int_coeff = 5203
+)(
+  input  clk,
+  input  rst_n,
+  input  s_axis_tvalid,
+  input  m_axis_tready,  // upstream device ready
+  input  signed [inout_width-1:0] s_axis_tdata,
+  output signed [inout_width-1:0] m_axis_tdata,
+  output m_axis_tvalid,
+  output s_axis_tready
+);
+
+  // filter coefficients (multiplied floating point coefficients by 2^scale_factor)
+  reg signed [coeff_width-1:0] b0_fixed = b0_int_coeff;   // g * b0 * 2^14  (multiply denom coeffs by gain for DF1 [source 1])
+  reg signed [coeff_width-1:0] b1_fixed = b1_int_coeff;   // g * b1 * 2^14 
+  reg signed [coeff_width-1:0] b2_fixed = b2_int_coeff;   // g * b2 * 2^14 
+  reg signed [coeff_width-1:0] a1_fixed = a1_int_coeff;
+  reg signed [coeff_width-1:0] a2_fixed = a2_int_coeff;
+
+  // input register
+  reg signed [inout_width-1:0] r_x = 0;
+
+  // output registers
+  reg r_m_axis_tvalid = 1'b0; 
+  reg r_s_axis_tready = 1'b0;
+  reg signed [inout_width-1:0] r_m_axis_tdata = 0;
+
+  // delay registers
+  reg signed [inout_width-1:0] r_x_z1 = 0;
+  reg signed [inout_width-1:0] r_x_z2 = 0;
+  reg signed [inout_width-1:0] r_y_z1 = 0;
+  reg signed [inout_width-1:0] r_y_z2 = 0;
+
+  // multiplication wires
+  wire signed [inout_width + coeff_width-1:0] w_product_b0;
+  wire signed [inout_width + coeff_width-1:0] w_product_b1;
+  wire signed [inout_width + coeff_width-1:0] w_product_b2;
+  wire signed [inout_width + coeff_width-1:0] w_product_a1;
+  wire signed [inout_width + coeff_width-1:0] w_product_a2;
+
+  // acummulate wire
+  wire signed [inout_width + coeff_width-1:0] w_sum; 
+
+  // states
+  localparam READY = 1'b0;
+  localparam BUSY  = 1'b1;
+
+  // state registers
+  reg r_current_state = READY;
+  reg r_next_state    = BUSY; 
+
+  // control signals
+  reg r_iir_en = 1'b0;  // enables iir filter
+
+  // next state logic
+  always @ (*)
+    begin 
+      case (r_current_state)
+        READY : 
+          begin 
+            if (s_axis_tvalid & s_axis_tready) // axis handshake. Incoming data not valid unless BOTH valid and ready are high
+              r_next_state <= BUSY;
+            else 
+              r_next_state <= r_current_state;
+          end 
+
+        BUSY : 
+          begin 
+            if (m_axis_tvalid == 1'b1)
+              r_next_state <= READY;
+            else 
+              r_next_state <= r_current_state;
+          end
+
+        default : r_next_state <= READY;
+      endcase
+    end
+
+  // output logic
+  always @ (*)
+    begin 
+      if (r_current_state == READY)
+        begin 
+          r_m_axis_tvalid <= 1'b0;  // data not valid
+          r_s_axis_tready <= 1'b1;  // set ready signal 
+          r_iir_en        <= 1'b0;  // dont enable iir filter in this state (downstream data not valid yet)
+        end 
+      else if (r_current_state == BUSY)
+        begin 
+          r_m_axis_tvalid <= 1'b1;  // data valid
+          r_s_axis_tready <= 1'b0;  // disable ready signal 
+          r_iir_en        <= 1'b1;  // iir filter enabled (MAC happens in one clock cycle)
+        end 
+      else 
+        begin 
+          r_m_axis_tvalid <= 1'b0;  
+          r_s_axis_tready <= 1'b0;  
+          r_iir_en        <= 1'b0;      
+        end   
+    end 
+
+  // state update
+  always @ (posedge clk, negedge rst_n)
+    begin
+      if (~rst_n)
+        r_current_state <= READY;
+      else 
+        r_current_state <= r_next_state;
+    end
+    
+  // AXIS handshake to upstream module
+  always @ (posedge clk, negedge rst_n)
+    begin
+      if (~rst_n)
+        r_m_axis_tdata <= 0;
+      else 
+        begin
+          if (m_axis_tready)  // only send data if upstream device is ready
+            r_m_axis_tdata <= r_y_z1;
+        end 
+    end  
+            
+  // iir delay element control
+  always @ (posedge clk, negedge rst_n)
+    begin
+        if (~rst_n)
+          begin 
+            r_x    <= 0; 
+            r_x_z1 <= 0; 
+            r_x_z2 <= 0;
+            r_y_z1 <= 0;
+            r_y_z2 <= 0;
+          end 
+        else 
+          begin
+            if (r_iir_en) 
+              begin 
+                r_x    <= s_axis_tdata;
+                r_x_z1 <= r_x;
+                r_x_z2 <= r_x_z1;
+                r_y_z1 <= w_sum >>> scale_factor;  // divide by the same 2^scale_factor value the coefficients were multiplied by
+                r_y_z2 <= r_y_z1;
+              end 
+          end 
+    end
+
+  // multiply
+  assign w_product_b0 = r_x    * b0_fixed;
+  assign w_product_b1 = r_x_z1 * b1_fixed;
+  assign w_product_b2 = r_x_z2 * b2_fixed;
+  assign w_product_a1 = r_y_z1 * -a1_fixed;
+  assign w_product_a2 = r_y_z2 * -a2_fixed;
+
+  // accumulate
+  assign w_sum = w_product_b0 + w_product_b1 + w_product_b2 + w_product_a1 + w_product_a2;
+
+  // output assignments
+  assign m_axis_tdata  = r_m_axis_tdata;
+  assign m_axis_tvalid = r_m_axis_tvalid;
+  assign s_axis_tready = r_s_axis_tready;
+
+endmodule
---
+++ AXI-Stream Biquad/iir_DF1_axis_tb.sv
@@ -0,0 +1,169 @@
+`timescale 1ns / 1 ps
+
+module iir_DF1_axi_s_tb;
+  reg clk;
+  reg signed [15:0] s_axis_tdata;
+  reg rst_n; 
+  reg s_axis_tvalid;
+  reg m_axis_tready;  // upstream device ready
+  wire signed [15:0] m_axis_tdata;
+  wire m_axis_tvalid;
+  wire s_axis_tready;
+
+  // clk
+  always #10 clk = ~clk;  // 50 MHz clk
+
+  // constants for module instantiation
+  localparam coeff_width  = 16;
+  localparam inout_width  = 16;
+  localparam scale_factor = 14;
+  localparam b0_int_coeff = 2962;
+  localparam b1_int_coeff = 5615;
+  localparam b2_int_coeff = 2962;
+  localparam a1_int_coeff = -9362;
+  localparam a2_int_coeff = 5203;
+
+  // uut instance
+  iir_DF1_Biquad_AXIS #(
+    .coeff_width(coeff_width),
+    .inout_width(inout_width),
+    .scale_factor(scale_factor),
+    .b0_int_coeff(b0_int_coeff),
+    .b1_int_coeff(b1_int_coeff),
+    .b2_int_coeff(b2_int_coeff),
+    .a1_int_coeff(a1_int_coeff),
+    .a2_int_coeff(a2_int_coeff)
+  ) uut (
+    .clk(clk),
+    .rst_n(rst_n),
+    .s_axis_tvalid(s_axis_tvalid),
+    .s_axis_tdata(s_axis_tdata),
+    .m_axis_tready(m_axis_tready),
+    .m_axis_tdata(m_axis_tdata),
+    .m_axis_tvalid(m_axis_tvalid),
+    .s_axis_tready(s_axis_tready)
+  );
+  
+  // variables for tb stimulus
+  integer i_impulse_max = 0;
+  integer fid;
+  integer status;
+  integer sample; 
+  integer i;
+  integer j = 0;
+
+  // global status flags to know which mode I am checking in the testbench
+  bit checking_impulse_resp = 1'b0;  
+  bit checking_wave_output = 1'b0;
+  
+  localparam num_samples = 250;
+  
+  // 2D register to hold test waveform stimulus
+  reg signed [15:0] r_wave_sample [num_samples - 1:0];
+
+  // generates an impulse
+  task axis_impulse();
+    begin
+      checking_impulse_resp = 1'b1;         // indicate impulse response mode
+      i_impulse_max = 2**(inout_width-1)-1; // 2^(inout_width-1) because input is signed, so to make max positive number need MSB-1 (sign bit stays 0)
+      wait (rst_n == 1'b1)                  // wait for reset release
+      wait (clk == 1'b0) wait (clk == 1'b1) // wait for rising edge clk
+
+      if (s_axis_tready == 1'b1)            // if uut is ready to accept data
+        begin 
+          s_axis_tdata  = i_impulse_max;    // send out impulse
+          s_axis_tvalid = 1'b1;
+          #20
+          s_axis_tvalid = 1'b0;
+        end
+
+      #80
+      wait (clk == 1'b0) wait (clk == 1'b1) // wait for rising edge clk 
+      s_axis_tdata = 0;                     // data line (impulse) goes back to 0
+      s_axis_tvalid = 1'b1;
+      #20
+      s_axis_tvalid = 1'b0;
+
+      repeat(50)  // repeat for 50 samples @ fs = 10 MHz (impulse is back at 0)
+        begin
+          #80
+          wait (clk == 1'b0) wait (clk == 1'b1) // wait for rising edge clk
+          s_axis_tvalid = 1'b1;                 // valid flag every clock cycle
+          #20
+          s_axis_tvalid = 1'b0;
+        end
+      
+      checking_impulse_resp = 1'b0;
+    end
+  endtask
+
+  // file output for impulse response
+  initial 
+    begin
+        wait (checking_impulse_resp == 1'b1) // indicates in the impulse response section of tb
+        fid = $fopen("Impulse_response_output.txt","w");     // create or open file
+        while (checking_impulse_resp == 1'b1)
+          begin 
+            wait (m_axis_tvalid == 0) wait (m_axis_tvalid == 1); // wait for rising edge of master tvalid output
+            $fdisplay(fid,"%d",m_axis_tdata);                    // write output data to file
+          end 
+        $fclose(fid);
+    end
+
+  // random upstream device tready deassertion
+  // output should just stall while module internals continue. 
+  // output will update with current output data when the ready is back high, since there is no fifo buffer to hold unread axi data
+  initial
+    begin
+      wait (checking_wave_output == 1'b1)  // indicates in the waveform output section of tb
+      m_axis_tready = 1'b1; // upstream device ready
+      #4630
+      m_axis_tready = 1'b0;  // after some random time, upstream device is not ready
+      #5570
+      m_axis_tready = 1'b1;  // upstream device becomes ready again. 
+    end 
+
+  // main stimulus intial block     
+  initial 
+    begin
+    clk = 1'b0;
+    rst_n = 1'b0; 
+    s_axis_tdata = 0;
+    s_axis_tvalid = 1'b0;
+    m_axis_tready = 1'b1; // upstream device ready
+    #40
+    rst_n = 1'b1;
+    #40
+    axis_impulse();      // test impulse response first
+    #10000
+    rst_n = 1'b0;        // reset module to test an input signal
+    checking_wave_output = 1'b1;
+
+    // load samples from MATLAB test waveform stimulus into register
+    fid = $fopen("500kHz_sine_wave_with_noise.txt","r");
+    for (i = 0; i < num_samples; i = i + 1)
+      begin
+        status = $fscanf(fid,"%d\n",sample); 
+        r_wave_sample[i] = 16'(sample);
+      end
+    $fclose(fid);
+    
+    #1000
+    rst_n = 1'b1; // release reset
+    
+    repeat(num_samples)  // 10 MHz sampling frequency for 250 samples
+      begin 
+        #80
+        s_axis_tdata = r_wave_sample[j];      // input new sample
+        j = j + 1;
+        wait (clk == 1'b0) wait (clk == 1'b1) // wait for rising edge of clock
+        s_axis_tvalid = 1'b1;
+        #20
+        s_axis_tvalid = 1'b0;                 // tvalid only high for 1 clock cycle
+      end
+      #50000
+      $finish;
+    $finish;
+    end
+
+endmodule
